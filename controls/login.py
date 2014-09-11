51875c0be976b7046f082e43ad0f3ffd73ea4712
Laercio Serra <laercio.serra@gmail.com>
2014-09-10 21:49:06 -0300

    commit 5

--- controls/login.py
+++ controls/login.py
@@ -50,7 +50,7 @@ if is_email:
             print (mysf.include_div_s())
             print (mysf.include_messages('2', ' Welcome to My Expenses Report!'))
             print (mysf.include_pageheader('Expenses ',' Last payments'))
-            print (mysf.include_search_form())  # TODO: criar a função de exportar os dados da tabela para o excel
+            print (mysf.include_search_form())
             print (mysf.include_data_table_disable(s_fields, s_dt_tb))
             print (mysf.include_pagination())  # TODO: criar a função de paginação para resultados com mais de 20 linhas
             print (mysf.include_delete())
